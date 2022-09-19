f0620719c8bb7bb8c98ce30617e3f6167a86577a
순민 <tkksm2@gmail.com>
2022-09-19 22:54:12 +0900

    fix emotion error

--- src/Button/Button.styles.ts
+++ src/Button/Button.styles.ts
@@ -7,6 +7,49 @@ export interface ButtonType {
   disabled?: boolean;
 }
 
+const dynamicStyle = (props: any) => {
+  if (props.disabled) {
+    return css`
+      &.text--black {
+        background-color: inherit;
+        color: ${props.theme.colors.black};
+        border: 2px solid ${props.theme.colors.grey};
+        color: ${props.theme.colors.grey};
+        point-events: none;
+      }
+      &.text--white {
+        background-color: ${props.theme.colors.grey};
+        border: 2px solid ${props.theme.colors.grey};
+        color: ${props.theme.colors.white};
+        point-events: none;
+      }
+    `;
+  } else {
+    return css`
+      &.text--black {
+        background-color: inherit;
+        border: 2px solid ${props.theme.colors.black};
+        color: ${props.theme.colors.black};
+        &:hover,
+        &:focus {
+          cursor: pointer;
+          background-color: ${props.theme.colors.grey};
+        }
+      }
+      &.text--white {
+        background-color: ${props.theme.colors.black};
+        border: 2px solid ${props.theme.colors.black};
+        color: ${props.theme.colors.white};
+        &:hover,
+        &:focus {
+          cursor: pointer;
+          background-color: ${props.theme.colors.grey};
+        }
+      }
+    `;
+  }
+};
+
 export const Button = styled.button`
   display: inline-flex;
   align-items: center;
@@ -25,22 +68,5 @@ export const Button = styled.button`
     font-size: 16px;
     padding: 12px 24px;
   }
-
-  ${({ disabled }: ButtonType) => {
-    if (disabled) {
-      return css`
-        &.text--black {
-          background-color: inherit;
-          color: ${(props) => props.theme.colors.black};
-        }
-      `;
-    } else {
-      return css`
-        &.text--black {
-          background-color: inherit;
-          color: red;
-        }
-      `;
-    }
-  }}
+  ${dynamicStyle}
 `;
